84605ef969283ee7aa746f9a82bd0ce5300eed4d
donghoony <aru0504@naver.com>
2023-11-10 15:10:03 +0900

    feat(Menu, Category): 기본적인 메뉴 및 카테고리 작성
    메뉴와 카테고리를 분리해 메뉴가 카테고리를 가지게 한다. 추후 카테고리별로 판단해야하는 기능이 있어 이와 같이 작성한다.

---
+++ src/main/java/christmas/domain/menu/Menu.java
@@ -0,0 +1,30 @@
+package christmas.domain.menu;
+
+public enum Menu {
+    MUSHROOM_SOUP(Category.APPETIZER, "양송이수프", 6_000L),
+    TAPAS(Category.APPETIZER, "타파스", 5_500L),
+    CAESAR_SALAD(Category.APPETIZER, "시저샐러드", 8_000L),
+
+    T_BONE_STEAK(Category.MAIN_DISH, "티본스테이크", 55_000L),
+    BARBEQUE_RIB(Category.MAIN_DISH, "바비큐립", 54_000L),
+    SEAFOOD_PASTA(Category.MAIN_DISH, "해산물파스타", 35_000L),
+    CHRISTMAS_PASTA(Category.MAIN_DISH, "크리스마스파스타", 25_000L),
+
+    CHOCOLATE_CAKE(Category.DESSERT, "초코케이크", 15_000L),
+    ICE_CREAM(Category.DESSERT, "아이스크림", 5_000L),
+
+    ZERO_COKE(Category.BEVERAGE, "제로콜라", 3_000L),
+    RED_WINE(Category.BEVERAGE, "레드와인", 60_000L),
+    CHAMPAGNE(Category.BEVERAGE, "샴페인", 25_000L),
+    ;
+
+    private final Category category;
+    private final String name;
+    private final long price;
+
+    Menu(Category category, String name, long price) {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+    }
+}
